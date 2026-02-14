7674ffefeb5dbe7e905854a2baf0d2a580dfc477
alonewinds <alonewinds2024@163.com>
2026-02-14 22:29:24 +0800

    汉化:local Ip address setting

--- plugins/ip_address/i18n/ip_address_zh_CN.ts
+++ plugins/ip_address/i18n/ip_address_zh_CN.ts
@@ -4,25 +4,29 @@
 <context>
     <name>IpAddressPlugin</name>
     <message>
-        <location filename="../ip_address_plugin.cpp" line="74"/>
         <source>&lt;no interfaces selected&gt;</source>
         <translation>&lt;没有接口被选择&gt;</translation>
     </message>
     <message>
-        <location filename="../ip_address_plugin.cpp" line="89"/>
         <source>waiting for an external address...</source>
         <translation>正在获取外部地址...</translation>
+    </message>
+    <message>
+        <source>waiting for local address...</source>
+        <translation>正在获取本地地址...</translation>
+    </message>
+    <message>
+        <source>&lt;invalid response&gt;</source>
+        <translation>&lt;无效响应&gt;</translation>
     </message>
 </context>
 <context>
     <name>IpAddressPluginFactory</name>
     <message>
-        <location filename="../ip_address_plugin.cpp" line="110"/>
         <source>Displays local IP address(es) under clock.</source>
         <translation>在时钟下显示本地IP地址。</translation>
     </message>
     <message>
-        <location filename="../ip_address_plugin.hpp" line="55"/>
         <source>IP address</source>
         <translation>IP地址</translation>
     </message>
@@ -30,24 +34,28 @@
 <context>
     <name>plugin::ip::SettingsWidget</name>
     <message>
-        <location filename="../gui/settings_widget.ui" line="14"/>
         <source>Network</source>
         <translation>网络</translation>
     </message>
     <message>
-        <location filename="../gui/settings_widget.ui" line="20"/>
         <source>Display internal address(es)</source>
         <translation>显示内部地址</translation>
     </message>
     <message>
-        <location filename="../gui/settings_widget.ui" line="37"/>
         <source>Display external address</source>
         <translation>显示外部地址</translation>
     </message>
     <message>
-        <location filename="../gui/settings_widget.ui" line="76"/>
         <source>External address detection service</source>
         <translation>外部地址检测服务</translation>
+    </message>
+    <message>
+        <source>Display local address (real public IP)</source>
+        <translation>显示本地地址（真实公网IP）</translation>
+    </message>
+    <message>
+        <source>Show location</source>
+        <translation>显示归属地</translation>
     </message>
     <message>
         <source>Display in one line</source>
